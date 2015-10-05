589b3f9b49ef2feb5223cedba128d2ddea002247
Mike Tsao <mike@sowbug.com>
2015-10-04 18:36:49 -0700

    New broken stuff

---
+++ firmware/main.c
@@ -0,0 +1,76 @@
+/* Name: main.c
+ * Project: custom-class, a basic USB example
+ * Author: Christian Starkjohann
+ * Creation Date: 2008-04-09
+ * Tabsize: 4
+ * Copyright: (c) 2008 by OBJECTIVE DEVELOPMENT Software GmbH
+ * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
+ */
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>  /* for sei() */
+#include <util/delay.h>     /* for _delay_ms() */
+
+#include <avr/pgmspace.h>   /* required by usbdrv.h */
+#include "usbdrv.h"
+#include "requests.h"       /* The custom request numbers we use */
+
+/* ------------------------------------------------------------------------- */
+/* ----------------------------- USB interface ----------------------------- */
+/* ------------------------------------------------------------------------- */
+
+usbMsgLen_t usbFunctionSetup(uchar data[8])
+{
+usbRequest_t    *rq = (void *)data;
+static uchar    dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup returns */
+
+    if(rq->bRequest == CUSTOM_RQ_ECHO){ /* echo -- used for reliability tests */
+        dataBuffer[0] = rq->wValue.bytes[0];
+        dataBuffer[1] = rq->wValue.bytes[1];
+        dataBuffer[2] = rq->wIndex.bytes[0];
+        dataBuffer[3] = rq->wIndex.bytes[1];
+        usbMsgPtr = dataBuffer;         /* tell the driver which data to return */
+        return 4;
+    }else if(rq->bRequest == CUSTOM_RQ_SET_STATUS){
+        if(rq->wValue.bytes[0] & 1){    /* set LED */
+        }else{                          /* clear LED */
+        }
+    }else if(rq->bRequest == CUSTOM_RQ_GET_STATUS){
+        dataBuffer[0] = 7;
+        usbMsgPtr = dataBuffer;         /* tell the driver which data to return */
+        return 1;                       /* tell the driver to send 1 byte */
+    }
+    return 0;   /* default for not implemented requests: return no data back to host */
+}
+
+/* ------------------------------------------------------------------------- */
+
+int __attribute__((noreturn)) main(void)
+{
+  uchar   i;
+
+    wdt_enable(WDTO_1S);
+    /* Even if you don't use the watchdog, turn it off here. On newer devices,
+     * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
+     */
+    /* RESET status: all port bits are inputs without pull-up.
+     * That's the way we need D+ and D-. Therefore we don't need any
+     * additional hardware initialization.
+     */
+    usbInit();
+    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
+    i = 0;
+    while(--i){             /* fake USB disconnect for > 250 ms */
+        wdt_reset();
+        _delay_ms(1);
+    }
+    usbDeviceConnect();
+    sei();
+    for(;;){                /* main event loop */
+        wdt_reset();
+        usbPoll();
+    }
+}
+
+/* ------------------------------------------------------------------------- */
