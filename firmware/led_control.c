42b4e8f823278466ede6a61e2074337d25cfbb60
Mike Tsao <mike@sowbug.com>
2016-11-10 20:43:09 -0800

    Grrr, hack around flaky probably fake APA102C

--- firmware/led_control.c
+++ firmware/led_control.c
@@ -19,6 +19,9 @@ void UpdateLEDs() {
   if (leds_need_update) {
     leds_need_update = 0;
     apa102_setleds(led, LED_COUNT);
+#if STUPID_COUNTERFEIT_APA102C
+    apa102_setleds(led, LED_COUNT);
+#endif
   }
 }
 
