5373fbda33efbabed7f4f477e4e60dac74f376cb
Reilly Grant <reillyg@chromium.org>
2017-10-20 17:18:02 -0700

    Fix up mp_loop.sh programming script
    Adjust the path to wlctl so that it can be executed from the firmware
    directory. Increase the time to wait after programming for higher
    reliability.

--- firmware/tools/mp_loop.sh
+++ firmware/tools/mp_loop.sh
@@ -4,25 +4,25 @@ while true; do
 
   echo "Press a key..."
   read -n 1 -s
-  
+
   echo -n "Updating firmware..."
   make upload
-  sleep 1
+  sleep 2
   echo "done"
-  
+
   echo -n "Setting origins..."
-  wlctl --urls https://sowbug.github.io/weblight http://localhost:8000
+  ../commandline/wlctl --urls https://sowbug.github.io/weblight http://localhost:8000
   echo "done"
 
   echo -n "Updating sequence..."
-  wlctl 000100 000000
-  wlctl --save
+  ../commandline/wlctl 000100 000000
+  ../commandline/wlctl --save
   echo "done"
 
   echo -n "Regenerating serial number..."
-  wlctl --regenerate-serial
+  ../commandline/wlctl --regenerate-serial
   echo "done"
-  
+
   echo "OK to unplug!"
   echo
   echo
